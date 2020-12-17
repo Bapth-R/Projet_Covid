977dc1b423aa300cecfe06fa8be6ed3fb76d09c9
Bapth-R <76159854+Bapth-R@users.noreply.github.com>
2020-12-17 12:30:48 +0100

    Add files via upload

---
+++ script_corona.bat
@@ -0,0 +1,2 @@
+curl --location --request GET "https://www.coronavirus-statistiques.com/corostats/openstats/open_stats_coronavirus.csv" > fichier_coronavirus.csv
+:: Il faut modifier le chemin pour que ca marche
